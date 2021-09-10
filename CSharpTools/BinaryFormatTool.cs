a41e08d70c89b50fd490bb3a4bd44baf07cba4b7
FredySu <56098046+FredySu@users.noreply.github.com>
2021-09-10 16:32:06 +0800

    update FileIO and BinaryFormatTool

---
+++ CSharpTools/BinaryFormatTool.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CSharpTools
+{
+    public class BinaryFormatTool
+    {
+        public static byte[] Serialize<T>(T obj)
+        {
+            if (obj == null) return null;
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            MemoryStream memoryStream = new MemoryStream();
+            binaryFormatter.Serialize(memoryStream, obj);
+            byte[] bytes = memoryStream.GetBuffer();
+            memoryStream.Close();
+            return bytes;
+        }
+
+        public static T Deserialize<T>(byte[] bytes)
+        {
+            object obj = null;
+            if (bytes != null)
+            {
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+                MemoryStream memoryStream = new MemoryStream(bytes);
+                obj = binaryFormatter.Deserialize(memoryStream);
+                memoryStream.Close();
+            }
+            return (T)obj;
+        }
+    }
+}
---
+++ CSharpTools/FileIO.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSharpTools
+{
+    class FileIO
+    {
+        public static void Write<T>(string filename, T[] fileKeys)
+        {
+            using StreamWriter sw = new StreamWriter(filename);
+            foreach (var fileKey in fileKeys)
+            {
+                string jsonData = JsonConvert.SerializeObject(fileKey);
+                sw.WriteLine(jsonData);
+            }
+        }
+        public static T[] Read<T>(string filename)
+        {
+            List<T> fileKeys = new List<T>();
+
+            string line = "";
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    T fileKey = JsonConvert.DeserializeObject<T>(line);
+                    fileKeys.Add(fileKey);
+                }
+            }
+
+            return fileKeys.ToArray();
+        }
+    }
+}
